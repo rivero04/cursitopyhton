fe878f1d66c387a9dc7d72d3d390e46bd29a2a32
Nacho <146840051+rivero04@users.noreply.github.com>
2024-01-18 11:58:41 -0300

    :)

---
+++ datos-simples.py
@@ -0,0 +1,19 @@
+"string"      
+            # string "texto"        
+'string'
+
+"""Tus datos son:
+        nombre: nacho   
+        apellido: rivero"""   # string pero con salto de linea
+        
+'''Tus datos son:
+        nombre: nacho
+        apellido: rivero''' # string pero con salto de linea
+        
+40  # esto es "int" (numero enteros)
+
+40.2 # esto es un "float" (numeros con coma)
+
+False
+            # estos son "bool" (valores booleanos: True o False)
+True
